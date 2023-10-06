277b7d4bce52bea93d000eed5519da968f56969d
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2023-10-06 01:55:21 -0400

    idk what im doing

--- plugin/src/main/java/net/elytrium/limboapi/server/LimboPlayerImpl.java
+++ plugin/src/main/java/net/elytrium/limboapi/server/LimboPlayerImpl.java
@@ -261,7 +261,7 @@ public class LimboPlayerImpl implements LimboPlayer {
 
   @Override
   public void disconnect() {
-    LimboSessionHandlerImpl handler = (LimboSessionHandlerImpl) this.connection.getSessionHandlerForRegistry(StateRegistry.PLAY);
+    LimboSessionHandlerImpl handler = (LimboSessionHandlerImpl) this.connection.getActiveSessionHandler();
     if (handler != null) {
       if (this.plugin.hasLoginQueue(this.player)) {
         handler.disconnected();
@@ -281,7 +281,7 @@ public class LimboPlayerImpl implements LimboPlayer {
 
   @Override
   public void disconnect(RegisteredServer server) {
-    LimboSessionHandlerImpl handler = (LimboSessionHandlerImpl) this.connection.getSessionHandlerForRegistry(StateRegistry.PLAY);
+    LimboSessionHandlerImpl handler = (LimboSessionHandlerImpl) this.connection.getActiveSessionHandler();
     if (handler != null) {
       if (this.plugin.hasLoginQueue(this.player)) {
         handler.disconnected();
